b48180e6dd31b672885a7c471184998c554f2dd8
kivimango <dev@c0d3r.nhely.hu>
2017-05-04 01:46:24 +0200

    everytinh public

--- src/test/java/com/kivimango/sluggenerator/SlugGeneratorTest.java
+++ src/test/java/com/kivimango/sluggenerator/SlugGeneratorTest.java
@@ -13,38 +13,41 @@ import static org.junit.Assert.assertEquals;
 
 public class SlugGeneratorTest {
 
+    public void  SlugGeneratorTest() {
+    }
+
     private SlugGenerator slugGenerator = new SlugGenerator();
 
     @Test
-    void TestSlugLength() {
+    public void TestSlugLength() {
         int desiredLength = 7;
         String slugForBlogPost = slugGenerator.generate(SlugGenerator.GENERATE_FROM_NUMBERS_AND_LETTERS, desiredLength);
         assertEquals(slugForBlogPost.length(), desiredLength);
     }
 
     @Test
-    void SlugLengthShouldBeCuttedToLimit() {
+    public void SlugLengthShouldBeCuttedToLimit() {
         int tooLongLength = 4653;
         String slugForPicture = slugGenerator.generate(SlugGenerator.GENERATE_FROM_NUMBERS_AND_LETTERS, tooLongLength);
         assertEquals(slugForPicture.length(), SlugGenerator.LENGTH_LIMIT);
     }
 
     @Test
-    void SlugShouldContainOnlyNumbers() {
+    public void SlugShouldContainOnlyNumbers() {
         int desiredLength = 10;
         String nineGagPostSlug = slugGenerator.generate(SlugGenerator.GENERATE_ONLY_FROM_NUMBERS, desiredLength);
         assertEquals(true, nineGagPostSlug.matches("[0-9]+"));
     }
 
     @Test
-    void SlugShouldContainOnlyLetters() {
+    public void SlugShouldContainOnlyLetters() {
         int desiredLength = 12;
         String articleSlug = slugGenerator.generate(SlugGenerator.GENERATE_ONLY_FROM_LETTERS, desiredLength);
         assertEquals(true, articleSlug.matches("[a-zA-Z]+"));
     }
 
     @Test
-    void SlugShouldContainNumbersOrLetters() {
+    public void SlugShouldContainNumbersOrLetters() {
         int desiredLength = 23;
         String webShopProductSlug = slugGenerator.generate(SlugGenerator.GENERATE_FROM_NUMBERS_AND_LETTERS, desiredLength);
         assertEquals(true, webShopProductSlug.matches("[0-9a-zA-Z]+"));
